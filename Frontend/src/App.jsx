2b3dbf9e1a2726f0b2568dd329130f6dc784c7b7
Kartik Gupta <kg9818630@gmail.com>
2026-07-25 14:15:09 +0530

    Added Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar"
 import Login from "./pages/Login"
-
+import {Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <Login />
+    <Login></Login>
   )
 }
-
 export default App
